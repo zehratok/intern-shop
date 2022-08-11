ebc0029e5f9082a37a123adde92fe50b06d35bf2
Zehra Tok <49477192+zehratok@users.noreply.github.com>
2022-08-11 11:41:04 +0300

    Create error page

--- internShop/src/app/app-routing.module.ts
+++ internShop/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./starter/starter.module').then(m => m.StarterModule)
+  },
+  {
+    path: '**',
+    component: ErrorComponent
   },
 
 ];
--- internShop/src/app/app.module.ts
+++ internShop/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
