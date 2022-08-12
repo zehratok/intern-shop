7e9fa69e2968113cf3ce094bfe7606be1e580e88
Zehra Tok <49477192+zehratok@users.noreply.github.com>
2022-08-12 10:00:23 +0300

    Set the redirection from the registration screen to the login screen.

--- internShop/src/app/starter/register/register.component.ts
+++ internShop/src/app/starter/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SnackbarService } from 'src/app/services/snackbar.service';
 
 @Component({
   selector: 'app-register',
@@ -6,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.sass']
 })
 export class RegisterComponent implements OnInit {
-  constructor() { }
+
+  constructor(
+    private router: Router,
+    private snackbarService: SnackbarService,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  register(): void {
+    if (1) {
+      this.snackbarService.createSnackbar('success', 'Registration Successful! You can login now!');
+      this.router.navigate(['/']);
+
+    }
+  }
 }
