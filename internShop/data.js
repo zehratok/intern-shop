9d25f9640a8d3d81c4eb8ef5995eba3e947d7156
Zehra Tok <49477192+zehratok@users.noreply.github.com>
2022-08-14 23:45:43 +0300

    Create data.js for to use json server

---
+++ internShop/data.js
@@ -0,0 +1,48 @@
+module.exports = function () {
+    return {
+        products: [
+            {
+                id: 1,
+                image: "../../assets/images/phone.jpg",
+                title: 'Mobile Phone',
+                model: 'Oppo Rx17-Neo',
+                oldPrice: 8599,
+                price: 7599,
+
+            }, {
+                id: 2,
+                image: "../../assets/images/phone.jpg",
+                title: 'Mobile Phone',
+                model: 'Oppo Rx17-Neo',
+                oldPrice: 8599,
+                price: 7599,
+
+            }, {
+                id: 3,
+                image: "../../assets/images/phone.jpg",
+                title: 'Mobile Phone',
+                model: 'Oppo Rx17-Neo',
+                oldPrice: 8599,
+                price: 7599,
+
+            }, {
+                id: 4,
+                image: "../../assets/images/phone.jpg",
+                title: 'Mobile Phone',
+                model: 'Oppo Rx17-Neo',
+                oldPrice: 8599,
+                price: 7599,
+
+            }, {
+                id: 5,
+                image: "../../assets/images/phone.jpg",
+                title: 'Mobile Phone',
+                model: 'Oppo Rx17-Neo',
+                oldPrice: 8599,
+                price: 7599,
+
+            },
+        ]
+    }
+}
+
